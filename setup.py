2c62a2795f8beacd8d9e7c30b5d12abb3f9d2952
Jeffrey Klardie <j.klardie@amsterdam.nl>
2019-10-03 10:30:51 +0200

    Version bump

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ extra_requirements = {
 
 setup(
     name='datapunt-keycloak-oidc',
-    version='0.3',
+    version='0.4',
     license='Mozilla Public License 2.0',
 
     author='Datapunt Amsterdam',
